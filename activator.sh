d9f4da8bd55ef42559f068162927b09cc4f2645b
Aaron Wagner <aaronwagner@Aarons-Mac-Pro.local>
2020-09-16 17:26:50 +0200

    working version

--- activator.sh
+++ activator.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 #check DirectHW loaded
+directHWloaded=$(kextstat -b org.apple.dont.block.DirectHW);
 
 
-#pcieports=$(sudo ./lspci -nnvt);
+chmod +x pciutils/lspci
+chmod +x pciutils/setpci
 
-sys_info_tmp='Documents/temp_sys_info.txt';
-#rm -f ~/$sys_info_tmp
+tmp_system_profiler='tmp_system_profiler.txt';
+tmp_lspci='tmp_lspci.txt';
 
-#pcidata=$(system_profiler SPPCIDataType > ~/$sys_info_tmp);
+rm -f $tmp_system_profiler
+rm -f $tmp_lspci
+
+pcidata=$(system_profiler SPPCIDataType > $tmp_system_profiler);
+pcieports=$(sudo ./pciutils/lspci -nnvt > $tmp_lspci);
 
 # remove whitespaces
-#tr -d " \t" < ~/$sys_info_tmp > ~/Documents/temp_sys_info_tr.txt
+replaceName='tmp_sys_info_tr.txt';
+tr -d " \t" < $tmp_system_profiler > $replaceName
 
 # replace original file with cleanded file
-#rm -f ~/$sys_info_tmp
-#mv ~/Documents/temp_sys_info_tr.txt ~/$sys_info_tmp
+rm -f $tmp_system_profiler
+mv $replaceName $tmp_system_profiler
 
 
 index=1
@@ -37,10 +44,10 @@ while read line; do
 			fi
 		fi
 		second=$((second+1))
-		done < ~/$sys_info_tmp
+		done < $tmp_system_profiler
 	fi
 index=$((index+1))
-done < ~/$sys_info_tmp
+done < $tmp_system_profiler
 
 if [ true ]
 then
@@ -48,26 +55,31 @@ then
 	echo vendorID: $vendorID
 	echo deviceID: $deviceID
 
-	pcie_id="["${vendorID:2}":"${deviceID:2}"]"
+	pci_id="["${vendorID:2}":"${deviceID:2}"]"
 
-	echo pcie-id: $pcie_id
+	echo pci-id: $pci_id
 fi
 
+# scan tmp_lspci
+while read pci_line; do
+	if [[ "$pci_line" == *"$pci_id"* ]]; then
+		rootport=00:${$pci_line:2-3}
+	fi
+done < $tmp_lspci
 
-
-
+echo rootport: $rootport
 
 speed=2
-rootport=00:1
 
 
-#linkstatus=$(sudo ./setpci -s $rootport CAP_EXP+12.w);
-#echo '# Before: PCIe' $(( 0x$linkstatus & 15)).0 x$(( 0x$linkstatus >> 4 & 31))
+linkstatus=$(sudo ./pciutils/setpci -s $rootport CAP_EXP+12.w);
+echo '# Before: PCIe' $(( 0x$linkstatus & 15)).0 x$(( 0x$linkstatus >> 4 & 31))
 
-#sudo ./setpci -s $rootport CAP_EXP+30.w=$speed:F # Set Link Speed
-#sudo ./setpci -s $rootport CAP_EXP+10.w=20:20 # Start Retrain
+sudo ./pciutils/setpci -s $rootport CAP_EXP+30.w=$speed:F # Set Link Speed
+sudo ./pciutils/setpci -s $rootport CAP_EXP+10.w=20:20 # Start Retrain
 
-#linkstatus=$(sudo ./setpci -s $rootport CAP_EXP+12.w);
-#echo '# After: PCIe' $(( 0x$linkstatus & 15)).0 x$(( 0x$linkstatus >> 4 & 31))
+linkstatus=$(sudo ./pciutils/setpci -s $rootport CAP_EXP+12.w);
+echo '# After: PCIe' $(( 0x$linkstatus & 15)).0 x$(( 0x$linkstatus >> 4 & 31))
 
-#rm -f $sys_info_tmp
+rm -f $tmp_system_profiler
+rm -f $tmp_lspci
